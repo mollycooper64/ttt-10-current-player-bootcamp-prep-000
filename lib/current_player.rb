b43d355ebdfac3f92420553d49555c33a8f336bc
Molly Cooper <mollycooper64@gmail.com>
2020-05-10 14:34:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,9 @@ def turn_count(board)
 
 counter = 0
 
-  if board.each do |position| = "X" || position = "O"
+  if board.each do |position|
+
+    position = "X" || position = "O"
 
     counter += 1
 
